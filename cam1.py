10541567414729cf636b6521944dfec42ec0eb4b
Pitirli <168733678+Pitirli@users.noreply.github.com>
2024-07-08 13:07:39 +0300

    cam1.py

--- cam1.py
+++ cam1.py
@@ -19,8 +19,6 @@ def pwm_control(pin, frequency, duty_cycle_start, duty_cycle_end, delay):
     p.stop()
     GPIO.cleanup()
 
-red = [0, 0, 255]
-
 picam2 = Picamera2()
 config = picam2.create_preview_configuration(main={"size": (640, 480)})
 picam2.configure(config)
@@ -30,17 +28,7 @@ try:
     while True:
         frame = picam2.capture_array()
 
-        hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-        lowerLimit0 = np.array([0, 120, 70], dtype=np.uint8)
-        upperLimit0 = np.array([10, 255, 255], dtype=np.uint8)
-        mask0 = cv2.inRange(hsvImage, lowerLimit0, upperLimit0)
-
-        lowerLimit1 = np.array([170, 120, 70], dtype=np.uint8)
-        upperLimit1 = np.array([180, 255, 255], dtype=np.uint8)
-        mask1 = cv2.inRange(hsvImage, lowerLimit1, upperLimit1)
-
-        mask = mask0 + mask1
+        
 
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
@@ -67,4 +55,3 @@ try:
 finally:
     picam2.stop()
     cv2.destroyAllWindows()
-
