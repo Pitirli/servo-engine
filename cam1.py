fc2db8f6d48cef60064539087abc9644d8dbbdc4
Pitirli <168733678+Pitirli@users.noreply.github.com>
2024-07-08 14:14:35 +0300

    cam1.py

--- cam1.py
+++ cam1.py
@@ -1,3 +1,4 @@
+from picamera2 import Picamera2, 
 import cv2
 from PIL import Image
 import numpy as np
@@ -5,7 +6,6 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
-
 
 def pwm_control(pin, frequency, duty_cycle_start, duty_cycle_end, delay):
     GPIO.setup(pin, GPIO.OUT)
@@ -20,57 +20,56 @@ def pwm_control(pin, frequency, duty_cycle_start, duty_cycle_end, delay):
     p.stop()
     GPIO.cleanup()
 
-
 red = [0, 0, 255]
-cap = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = cap.read()
+# Initialize the camera
+picam2 = Picamera2()
+picam2.start_preview(Preview.QTGL)
+picam2.configure(picam2.preview_configuration(main={"format": "BGR888", "size": (640, 480)}))
+picam2.start()
 
-    if not ret:
-        break
+try:
+    while True:
+        frame = picam2.capture_array()
 
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+        lowerLimit0 = np.array([0, 120, 70], dtype=np.uint8)
+        upperLimit0 = np.array([10, 255, 255], dtype=np.uint8)
+        mask0 = cv2.inRange(hsvImage, lowerLimit0, upperLimit0)
 
-    lowerLimit0 = np.array([0, 120, 70], dtype=np.uint8)
-    upperLimit0 = np.array([10, 255, 255], dtype=np.uint8)
-    mask0 = cv2.inRange(hsvImage, lowerLimit0, upperLimit0)
+        lowerLimit1 = np.array([170, 120, 70], dtype=np.uint8)
+        upperLimit1 = np.array([180, 255, 255], dtype=np.uint8)
+        mask1 = cv2.inRange(hsvImage, lowerLimit1, upperLimit1)
 
-    lowerLimit1 = np.array([170, 120, 70], dtype=np.uint8)
-    upperLimit1 = np.array([180, 255, 255], dtype=np.uint8)
-    mask1 = cv2.inRange(hsvImage, lowerLimit1, upperLimit1)
+        mask = mask0 + mask1
 
-    mask = mask0 + mask1
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
 
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+        if contours:
+            largest_contour = max(contours, key=cv2.contourArea)
+            (x, y, w, h) = cv2.boundingRect(largest_contour)
+            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
 
+            M = cv2.moments(largest_contour)
+            if M["m00"] != 0:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                cv2.circle(frame, (cX, cY), 5, (0, 255, 0), -1)
 
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            pwm_control(11, 50, 3, 12, 1)
 
-    if contours:
+        cv2.imshow('frame', frame)
 
-        largest_contour = max(contours, key=cv2.contourArea)
-        (x, y, w, h) = cv2.boundingRect(largest_contour)
-        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
+finally:
+    picam2.stop()
+    cv2.destroyAllWindows()
 
-        M = cv2.moments(largest_contour)
-        if M["m00"] != 0:
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-            cv2.circle(frame, (cX, cY), 5, (0, 255, 0), -1)
-
-        pwm_control(11, 50, 3, 12, 1)
-
-    cv2.imshow('frame', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
 
